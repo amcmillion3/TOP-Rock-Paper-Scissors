9975d86382c60e2603aac6b67df87ab0b43302b2
Andrew McMillion <amcmillion3@gmail.com>
2021-05-01 12:29:48 +0200

    Update the game with a UI

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # TOP-Rock-Paper-Scissors
 
 This is the Rock Paper Scissors project from The Odin Project.
-
-The game is entirely console based. 
-
-When I learn the advanced JS techniques I may come back and update it with a visual interface. 
---
+++ app.js
@@ -0,0 +1,103 @@
+let playerScore = 0;
+let computerScore = 0;
+const playerScoreSpan = document.getElementById("player-score");
+const computerScoreSpan = document.getElementById("comp-score");
+let result = document.querySelector(".result > p");
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const resetBtn = document.querySelector(".reset-btn");
+
+// Computer Choice
+function generateComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let number = Math.floor(Math.random() * 3);
+    return choices[number];
+};
+
+// Capitalize words because I was dumb and forgot to when starting
+function capital(word) {
+    if(word === "rock") return "Rock";
+    if(word === "paper") return "Paper";
+    return "Scissors";
+};
+
+// Win function
+function win(playerChoice, computerChoice) {
+    const getPlayerChoiceDiv = document.getElementById(playerChoice);
+    playerScore ++;
+    playerScoreSpan.textContent = playerScore;
+    result.textContent = `${capital(playerChoice)} beats ${capital(computerChoice)}! You Win!`;
+    getPlayerChoiceDiv.classList.add("green-glow");
+    setTimeout(() => {
+        getPlayerChoiceDiv.classList.remove("green-glow");
+    }, 300);
+};
+
+// Lose function
+function lose(playerChoice, computerChoice) {
+    const getPlayerChoiceDiv = document.getElementById(playerChoice);
+    computerScore ++;
+    computerScoreSpan.textContent = computerScore;
+    result.textContent = `${capital(computerChoice)} beats ${capital(playerChoice)}! You Lose!`;
+    getPlayerChoiceDiv.classList.add("red-glow");
+    setTimeout(() => {
+        getPlayerChoiceDiv.classList.remove("red-glow");
+    }, 300);
+};
+
+function draw(playerChoice) {
+    const getPlayerChoiceDiv = document.getElementById(playerChoice);
+    result.textContent = `Draw. Please play again.`;
+    getPlayerChoiceDiv.classList.add("gray-glow");
+    setTimeout(() => {
+        getPlayerChoiceDiv.classList.remove("gray-glow");
+    }, 300);
+};
+
+// One Round of the game
+function game(playerChoice) {
+    const computerChoice = generateComputerChoice();
+    switch(playerChoice + computerChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            win(playerChoice, computerChoice);
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            lose(playerChoice, computerChoice);
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw(playerChoice);
+            break;
+    };
+};
+
+// Player choice
+function playerChoice() {
+    rockBtn.addEventListener("click", () => {
+        game("rock");
+    });
+    paperBtn.addEventListener("click", () => {
+        game("paper");
+    });
+    scissorsBtn.addEventListener("click", () => {
+        game("scissors");
+    });
+};
+
+playerChoice();
+
+// reset button
+resetBtn.addEventListener("click", () => {
+    reset();
+});
+
+function reset() {
+    playerScoreSpan.innerHTML = 0;
+    computerScoreSpan.innerHTML = 0;
+};
